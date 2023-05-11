78524f90553cc38c619269c1e6878738b0f1ead6
Vikram Kumar Meena <vikramkr.meena.min16@itbhu.ac.in>
2023-05-11 16:01:21 +0530

    changes to connectedDb

--- connectToDB.js
+++ connectToDB.js
@@ -1,8 +1,8 @@
 import mysql from "mysql";
 
 export const db = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "VIKRAM123",
-    database: "social"
+    host: process.env.DB_HOST,
+    user: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DBNAME
 });
